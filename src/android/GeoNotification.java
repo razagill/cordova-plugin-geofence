e2719e056fcc58c8323c71ad6219b0938d3b6a23
Raza Gill <raza.gill@outlook.com>
2016-02-23 11:18:20 +0100

    added new receiver to post a url with the notification data that will be executed

--- src/android/GeoNotification.java
+++ src/android/GeoNotification.java
@@ -2,6 +2,7 @@ package com.cowbell.cordova.geofence;
 
 import com.google.android.gms.location.Geofence;
 import com.google.gson.annotations.Expose;
+import java.util.*;
 
 public class GeoNotification {
     @Expose public String id;
@@ -9,6 +10,9 @@ public class GeoNotification {
     @Expose public double longitude;
     @Expose public int radius;
     @Expose public int transitionType;
+
+    @Expose public String url;
+    @Expose public Map<String, String> headers;
 
     @Expose public Notification notification;
 
